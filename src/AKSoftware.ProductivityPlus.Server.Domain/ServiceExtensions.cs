99f0bf8f42fe09e2302b172f3326082aa5d2ed86
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-09-17 23:48:14 +0400

    Create the Azure Functions project for the API

---
+++ src/AKSoftware.ProductivityPlus.Server.Domain/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKSoftware.ProductivityPlus.Server.Domain;
+
+public static class ServiceExtensions
+{
+
+	public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration config)
+	{
+		return services.AddSingleton(sp => new CosmosClient(config["CosmosDb:ConnectionString"], new()
+		{
+			AllowBulkExecution = true
+		}));
+	}
+
+}
